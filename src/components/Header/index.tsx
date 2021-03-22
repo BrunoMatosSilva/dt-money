55159546c8a5ff392d15dbcfd7290be1b258f1b3
BrunoMatosSilva <brunomatos2010@outlook.com>
2021-03-21 22:48:24 -0300

    styles modal

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,26 @@
+
 import logoImg from '../../assets/logo.svg';
 
 import { Container, Content } from './styles'
 
-export function Header() {
+interface HeaderProps {
+    onOpenNewtransactionModal: () => void;
+}
+
+export function Header({ onOpenNewtransactionModal }: HeaderProps) {
+
     return (
         <Container>
             <Content>
                 <img src={logoImg} alt="dt money" />
-                <button type="button">Nova transação</button>
+                <button
+                    type="button"
+                    onClick={onOpenNewtransactionModal}
+                >
+                    Nova transação
+                </button>
+
+
             </Content>
         </Container>
     );
